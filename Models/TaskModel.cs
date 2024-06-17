9df6a3ecd33638e323d3d39efd444e2eecf311e4
Isac Vieira <isacvieira2009@hotmail.com>
2024-06-17 18:56:16 -0300

    initial

---
+++ Models/TaskModel.cs
@@ -0,0 +1,9 @@
+namespace Tarefas.Models
+{
+    public class TaskModel
+    {
+        public string TaskTittle { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime TaskDeadline { get; set; }
+    }
+}
---
+++ Views/Home/Home.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tarefas.Views.Home
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
