fd9a64f5d6b970e9cd137450334b6946c62aace9
Isac Vieira <isacvieira2009@hotmail.com>
2024-06-17 19:32:52 -0300

    refatorando DI

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,9 +14,6 @@ namespace Tarefas.Controllers
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
     }
 }
